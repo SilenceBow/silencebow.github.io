863a5e0e1bdd1c7c6bfb39c8221a08ec1873cafc
silencebow <29562312+silencebow@users.noreply.github.com>
2021-03-06 13:12:18 -0700

    Turn the object into a basic array
    Also insert final newlines

--- assets/scripts/jquote_array.js
+++ assets/scripts/jquote_array.js
@@ -1,237 +1,48 @@
 var quotes = [
-  {
-    quote: "I would not chug this Mountain Dew Throwback for a million dollars.",
-    link: "https://youtu.be/Lm_XqaFh1uY?t=859",
-    duration: "5000"
-  },
-  {
-    quote: "I drank like three or four little chugs of this and my balls hurt. Right now. It has to be related.",
-    link: "https://youtu.be/Lm_XqaFh1uY?t=951",
-    duration: "10000"
-  },
-  {
-    quote: "I value non-testicular pain.",
-    link: "https://youtu.be/Lm_XqaFh1uY?t=968",
-    duration: "5000"
-  },
-  {
-    quote: "Hey Mr. President, uhh, can you like chug this Mountain Dew and shit your pants?",
-    link: "https://youtu.be/Lm_XqaFh1uY?t=1008",
-    duration: "6000"
-  },
-  {
-    quote: "When Abraham Lincoln fuckin' created the universe this is not what he thought was gonna happen.",
-    link: "https://youtu.be/Lm_XqaFh1uY?t=1123",
-    duration: "5000"
-  },
-  {
-    quote: "No, I'm not gonna turn this into like a fetish stream. I'm not doing that.",
-    link: "https://youtu.be/Lm_XqaFh1uY?t=1430",
-    duration: "9000"
-  },
-  {
-    quote: "What, you're telling me I just can't slide like a DVD player into my pants and walk out? Why not?",
-    link: "https://youtu.be/Lm_XqaFh1uY?t=1749",
-    duration: "9000"
-  },
-  {
-    quote: "No that's actually true, I'm not making that up, that's real. This is like the 5% time (sic) when I'm saying something real.",
-    link: "https://youtu.be/Lm_XqaFh1uY?t=2222",
-    duration: "0"
-  },
-  {
-    quote: "Welcome to Dark Souls games, if your back's in a corner, you may as well go microwave a fuckin' burrito.",
-    link: "https://youtu.be/8Kpu58900Kk?t=220",
-    duration: "8000"
-  },
-  {
-    quote: "He kicked me, Mom, he kicked me in the face from across the fucking game!",
-    link: "https://youtu.be/8Kpu58900Kk?t=408",
-    duration: "5000"
-  },
-  {
-    quote: "Yeah, take a picture of my mouth!",
-    link: "https://youtu.be/olJq4Vp-64M?t=518",
-    duration: "2000"
-  },
-  {
-    quote: "Hey, get ready for some fake reactions.",
-    link: "https://youtu.be/ZdnRu3PLZ-U?t=9",
-    duration: "3000"
-  },
-  {
-    quote: "Suck my fuckin' bum.",
-    link: "https://youtu.be/ZdnRu3PLZ-U?t=815",
-    duration: "3000"
-  },
-  {
-    quote: "Maybe like 5% of you are chumps.",
-    link: "https://youtu.be/ZdnRu3PLZ-U?t=917",
-    duration: "3000"
-  },
-  {
-    quote: "You ever shoved a wrench up your ass?",
-    link: "https://youtu.be/xH7fcqrvEIY?t=424",
-    duration: "3000"
-  },
-  {
-    quote: "Shit in my face.",
-    link: "https://youtu.be/KKxq7OxBsoY?t=361",
-    duration: "3000"
-  },
-  {
-    quote: "And my hand is just caked with shit.",
-    link: "https://youtu.be/HR4cGhdd1Ds?t=2852",
-    duration: "5000"
-  },
-  {
-    quote: "Im on crack today, I'm gonna be honest with you.",
-    link: "https://youtu.be/YaS8KkfLHA8?t=510",
-    duration: "3000"
-  },
-  {
-    quote: "Hey, shove those bone dragons up your ass, you fuckin' dick.",
-    link: "https://youtu.be/L27Lmrnqc_0?t=1140",
-    duration: "5000"
-  },
-  {
-    quote: "Pineapple on pizza is fucking disgusting.",
-    link: "https://youtu.be/WBdtiBcCgoE?t=560",
-    duration: "4000"
-  },
-  {
-    quote: "The Witcher 3 is not a good game at all. I think it should never have received even one award.",
-    link: "https://youtu.be/WBdtiBcCgoE?t=592",
-    duration: "7000"
-  },
-  {
-    quote: "Half-Life 1 and Half-Life 2, like I mean really that whole company in general just made nothing but 4/10 games.",
-    link: "https://youtu.be/WBdtiBcCgoE?t=605",
-    duration: "7000"
-  },
-  {
-    quote: "No, I'm not going to shave my armpits. I manage my armpits.",
-    link: "https://youtu.be/WBdtiBcCgoE?t=693",
-    duration: "6000"
-  },
-  {
-    quote: "Don't you hate it when you have chicken bones in your coffee?",
-    link: "https://youtu.be/uVBZew0eLQY?t=350",
-    duration: "2000"
-  },
-  {
-    quote: "Coffee, Cheetos, Chicken.",
-    link: "https://youtu.be/6x7ezLiR4rY?t=0",
-    duration: "0"
-  },
-  {
-    quote: "Get ready for a real jock-shocker.",
-    link: "https://youtu.be/ydo2GNXEh2E?t=420",
-    duration: "3000"
-  },
-  {
-    quote: "Hey, how about you suck a fart outta my ass.",
-    link: "https://youtu.be/ydo2GNXEh2E?t=563",
-    duration: "3000"
-  },
-  {
-    quote: "Do you eat ass?",
-    link: "https://youtu.be/dEU005uOMlk?t=211",
-    duration: "3000"
-  },
-  {
-    quote: "Do you know what it's like to shit?",
-    link: "unknown",
-    duration: "0"
-  },
-  {
-    quote: "I'm hiding my penis because people are outta control!",
-    link: "https://youtu.be/pbMkyQdo2x8?t=1032",
-    duration: "6000"
-  },
-  {
-    quote: "I sound like an attitude boy.",
-    link: "https://youtu.be/4-QMpR1L6XM?t=158",
-    duration: "3000"
-  },
-  {
-    quote: "Jell-O is somebody spitting a cold loogie down your throat.",
-    link: "https://youtu.be/PHUjZ91boic?t=213",
-    duration: "5000"
-  },
-  {
-    quote: "Everybody knows at Burger King there are fuckin' ants everywhere.",
-    link: "https://youtu.be/_3Mg3x5xsmU?t=60",
-    duration: "7000"
-  },
-  {
-    quote: "I can't call Luigi anymore, he called up Verizon and had my number fuckin' blocked. Permanently.",
-    link: "https://youtu.be/DtXwqjz0MYE?t=436",
-    duration: "9000"
-  },
-  {
-    quote: "Am I the porn?",
-    link: "https://youtu.be/aprn0dzKHww?t=78",
-    duration: "2000"
-  },
-  {
-    quote: "Why can't we just eat shit?",
-    link: "https://youtu.be/CUUMJljcFAQ?t=65",
-    duration: "3000"
-  },
-  {
-    quote: "I'm on drugs you motherfuckers!",
-    link: "https://youtu.be/X2C9dSRi9S8?t=282",
-    duration: "3000"
-  },
-  {
-    quote: "I would suck to be him.",
-    link: "https://youtu.be/tPKfNqTgvfE?t=541",
-    duration: "2000"
-  },
-  {
-    quote: "If you can't accept me at my worst, obey your thirst.",
-    link: "https://youtu.be/h8lwnXjGZ5s?t=2",
-    duration: "5000"
-  },
-  {
-    quote: "It's over!",
-    link: "https://youtu.be/N_gwh2JDdwE?t=25",
-    duration: "2000"
-  },
-  {
-    quote: "I wake up, I look at the ceiling, I go 'Yo! Merry Christmas!', and then I diarrhea. Every single morning.",
-    link: "https://youtu.be/_DsGrb-tJI0?t=362",
-    duration: "7000"
-  },
-  {
-    quote: "You can pee on me all you want, I don't care.",
-    link: "https://youtu.be/otrkNH3FFcg?t=5913",
-    duration: "3000"
-  },
-  {
-    quote: "Piss on me, piss in my face!",
-    link: "https://youtu.be/otrkNH3FFcg?t=5917",
-    duration: "3000"
-  },
-  {
-    quote: "Gonna shit in everybody is this room's cereal.",
-    link: "https://youtu.be/CKsW2g0NZMs?t=36",
-    duration: "4000"
-  },
-  {
-    quote: "If I don't get three headshots in a row right now, I'm going to reach into the toilet and eat my own shit.",
-    link: "https://youtu.be/CKsW2g0NZMs?t=43",
-    duration: "8000"
-  },
-  {
-    quote: "I'll be back, I gotta go eat my own shit.",
-    link: "https://youtu.be/CKsW2g0NZMs?t=57",
-    duration: "3000"
-  },
-  // {
-  //   quote: "This is the claimed video for error testing",
-  //   link: "https://youtu.be/HR4cGhdd1Ds?t=2852",
-  //   duration: "5000"
-  // },
+  "I would not chug this Mountain Dew Throwback for a million dollars.", 
+  "I drank like three or four little chugs of this and my balls hurt. Right now. It has to be related.", 
+  "I value non-testicular pain.", 
+  "Hey Mr. President, uhh, can you like chug this Mountain Dew and shit your pants?", 
+  "When Abraham Lincoln fuckin' created the universe this is not what he thought was gonna happen.", 
+  "No, I'm not gonna turn this into like a fetish stream. I'm not doing that.", 
+  "What, you're telling me I just can't slide like a DVD player into my pants and walk out? Why not?", 
+  "No that's actually true, I'm not making that up, that's real. This is like the 5% time (sic) when I'm saying something real.", 
+  "Welcome to Dark Souls games, if your back's in a corner, you may as well go microwave a fuckin' burrito.", 
+  "He kicked me, Mom, he kicked me in the face from across the fucking game!", 
+  "Yeah, take a picture of my mouth!", 
+  "Hey, get ready for some fake reactions.", 
+  "Suck my fuckin' bum.", 
+  "Maybe like 5% of you are chumps.", 
+  "You ever shoved a wrench up your ass?", 
+  "Shit in my face.", 
+  "And my hand is just caked with shit.", 
+  "Im on crack today, I'm gonna be honest with you.", 
+  "Hey, shove those bone dragons up your ass, you fuckin' dick.", 
+  "Pineapple on pizza is fucking disgusting.", 
+  "The Witcher 3 is not a good game at all. I think it should never have received even one award.", 
+  "Half-Life 1 and Half-Life 2, like I mean really that whole company in general just made nothing but 4/10 games.", 
+  "No, I'm not going to shave my armpits. I manage my armpits.", 
+  "Don't you hate it when you have chicken bones in your coffee?", 
+  "Coffee, Cheetos, Chicken.", 
+  "Get ready for a real jock-shocker.", 
+  "Hey, how about you suck a fart outta my ass.", 
+  "Do you eat ass?", 
+  "Do you know what it's like to shit?", 
+  "I'm hiding my penis because people are outta control!", 
+  "I sound like an attitude boy.", 
+  "Jell-O is somebody spitting a cold loogie down your throat.", 
+  "Everybody knows at Burger King there are fuckin' ants everywhere.", 
+  "I can't call Luigi anymore, he called up Verizon and had my number fuckin' blocked. Permanently.", 
+  "Am I the porn?", 
+  "Why can't we just eat shit?", 
+  "I'm on drugs you motherfuckers!", 
+  "I would suck to be him.", 
+  "If you can't accept me at my worst, obey your thirst.", 
+  "It's over!", 
+  "I wake up, I look at the ceiling, I go 'Yo! Merry Christmas!', and then I diarrhea. Every single morning.", 
+  "You can pee on me all you want, I don't care.", 
+  "Piss on me, piss in my face!", 
+  "Gonna shit in everybody is this room's cereal.", 
+  "If I don't get three headshots in a row right now, I'm going to reach into the toilet and eat my own shit.", 
+  "I'll be back, I gotta go eat my own shit.", 
 ];
